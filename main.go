3048cd881f34c15e41353a0b961a0c058ed1b3d5
haynb <2587971441@qq.com>
2023-08-24 10:35:35 +0800

    稳定版本，核定代码

--- main.go
+++ main.go
@@ -3,50 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/tmc/langchaingo/documentloaders"
+	"github.com/tmc/langchaingo/textsplitter"
 	"io"
+	"os"
 	"qdrant/db"
 	"qdrant/gpt"
 )
 
 func main() {
-	//file, err := os.Open("666.pdf")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//// 获取文件大小
-	//fileInfo, err := file.Stat()
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
-	//fileSize := fileInfo.Size()
-	//p := documentloaders.NewPDF(file, fileSize)
-	//spliter := textsplitter.NewTokenSplitter()
-	//doc, err := p.LoadAndSplit(context.Background(), spliter)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("文件已经加载完毕")
+	file, err := os.Open("666.pdf")
+	if err != nil {
+		panic(err)
+	}
+	// 获取文件大小
+	fileInfo, err := file.Stat()
+	if err != nil {
+		panic(err)
+		return
+	}
+	fileSize := fileInfo.Size()
+	p := documentloaders.NewPDF(file, fileSize)
+	spliter := textsplitter.NewTokenSplitter()
+	doc, err := p.LoadAndSplit(context.Background(), spliter)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("文件已经加载完毕")
 	e, err := gpt.NewEm()
 	if err != nil {
 		panic(err)
 	}
-	//points, err := db.DocToPoints(doc, e)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//err = db.DeleteCollection("test")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//_, err = db.CreateCollection("test")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//_, err = db.AddPoints("test", points)
-	//if err != nil {
-	//	panic(err)
-	//}
+	points, err := db.DocToPoints(doc, e)
+	if err != nil {
+		panic(err)
+	}
+	err = db.DeleteCollection("test")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.CreateCollection("test")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.AddPoints("test", points)
+	if err != nil {
+		panic(err)
+	}
 	query := "赛维周边环境怎么样？"
 	q_e, err := e.EmbedQuery(context.Background(), query)
 	if err != nil {
