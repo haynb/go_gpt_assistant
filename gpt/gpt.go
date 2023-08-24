e4218e074c54b4d4acd315c7f34c6c85025e2d3a
he anyang <124337128+haynb@users.noreply.github.com>
2023-08-24 10:39:24 +0800

    Update gpt.go

--- gpt/gpt.go
+++ gpt/gpt.go
@@ -18,13 +18,13 @@ type Gpt3Dot5Message openai.ChatCompletionMessage
 func init() {
 	//定义要设置的环境变量和对应的值
 	envVars := map[string]string{
-		"OPENAI_API_KEY":  "sk-UhzxW8avagN8D00Q2AmHeJL1LBz6NFv9rI3USa94GiXGPa9r",
-		"OPENAI_BASE_URL": "https://cfwus02.opapi.win/v1",
+		"OPENAI_API_KEY":  "sk-Uhz**************************************GiXGPa9r",
+		"OPENAI_BASE_URL": "https://cfw***********pi.win/v1",
 		"OPENAI_MODEL":    "gpt-3.5-turbo",
 	}
 	//envVars := map[string]string{
-	//	"OPENAI_API_KEY":  "sk-ql6RVY09mGrVRsbS7b1hEwsmTRXSxjttK17P5wPl3LWlR2Ek",
-	//	"OPENAI_BASE_URL": "https://gptgod.site/api/v1",
+	//	"OPENAI_API_KEY":  "sk-ql6RVY***************************R2Ek",
+	//	"OPENAI_BASE_URL": "https://gp****************/v1",
 	//	"OPENAI_MODEL":    "gpt-3.5-turbo",
 	//}
 	// 逐个设置环境变量
@@ -46,8 +46,8 @@ func NewEm() (em.OpenAI, error) {
 func NewChatGptTool() *ChatGptTool {
 	//key := os.Getenv("OPENAI_API_KEY")
 	//url := os.Getenv("OPENAI_BASE_URL")
-	key := "sk-ql6RVY09mGrVRsbS7b1hEwsmTRXSxjttK17P5wPl3LWlR2Ek"
-	url := "https://gptgod.online/api/v1"
+	key := "sk-ql6RVY******************************5wPl3LWlR2Ek"
+	url := "https://g***************ne/api/v1"
 	config := openai.DefaultConfig(key)
 	config.BaseURL = url
 	client := openai.NewClientWithConfig(config)
